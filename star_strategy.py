73272b930192153e0f359f362d6b9ba26269b3f0
zoupeng.zp <zoupeng.zp@alibaba-inc.com>
2021-12-23 17:49:18 +0800

    ad

--- star_strategy.py
+++ star_strategy.py
@@ -1,8 +1,72 @@
 from result import Result
+import talib as ta
 
 
-def morning_star(klines,range):
-    return Result()
+def morning_star(klines,range=0.0005):
+    days = 12
+    ma = ta.MA(klines['close'], timeperiod=days)
+    results = []
+
+    for index in range(len(klines)):
+        if index < days:
+            continue
+        today = klines.iloc[index]
+
+        if today['open'] * (1+range) > today['close'] > today['open'] * (1-range):
+
+            satisfy_ma = True
+            # 连续days天ma趋势线都是下跌形态
+            for ma_index in range(days):
+                if ma_index == 0:
+                    continue
+                satisfy_ma = ma[index] < ma[index - ma_index]
+                if not satisfy_ma:
+                    break;
+            if satisfy_ma:
+                satisfy_low = True;
+                for body_index in range(days):
+                    if body_index == 0:
+                        continue
+                    satisfy_low = today['open'] < min(klines.iloc[index - body_index]['close'],
+                                                      klines.iloc[index - body_index]['open'])
+                    if not satisfy_low:
+                        break;
+                if satisfy_low:
+                    results.append(
+                        Result(today['code'], 'BUY', today['close'], today['time_key'], 'swallow').get_dict())
+    return results
 
 def evening_star(klines,range):
-    return Result()
+    days = 12
+    ma = ta.MA(klines['close'], timeperiod=days)
+    results = []
+
+    for index in range(len(klines)):
+        if index < days:
+            continue
+        today = klines.iloc[index]
+
+        if today['open'] * (1+range) > today['close'] > today['open'] * (1-range):
+
+            satisfy_ma = True
+            # 连续days天ma趋势线都是上涨形态
+            for ma_index in range(days):
+                if ma_index == 0:
+                    continue
+                satisfy_ma = ma[index] > ma[index - ma_index]
+                if not satisfy_ma:
+                    break;
+            if satisfy_ma:
+                satisfy_high = True;
+                for body_index in range(days):
+                    if body_index == 0:
+                        continue
+                    satisfy_high = today['open'] > max(klines.iloc[index - body_index]['close'],
+                                                       klines.iloc[index - body_index]['open'])
+                    if not satisfy_high:
+                        break;
+                if satisfy_high:
+                    results.append(
+                        Result(today['code'], 'SELL', today['close'], today['time_key'], 'swallow').get_dict())
+
+    return results
