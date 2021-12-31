92640e2639114c3918504a50bb7292763b0d5782
zoupeng.zp <zoupeng.zp@alibaba-inc.com>
2021-12-31 15:43:41 +0800

    ad

---
+++ config.py
@@ -0,0 +1,2 @@
+jqdata_username='18602842845'
+jqdata_password='Cd02884077655'
--- use_jqdata.py
+++ use_jqdata.py
@@ -3,6 +3,7 @@ import time
 import jqdatasdk as jq
 import pandas as pd
 
+import config
 import test
 import util
 from market.chinese import shanghai_a
@@ -14,7 +15,7 @@ if __name__ == '__main__':
     pd.set_option('display.max_colwidth', 1000)
     pd.set_option('display.width', 1000)
 
-    jq.auth(username="18602842845",password="Cd02884077655")
+    jq.auth(username=config.jqdata_username,password=config.jqdata_password)
 
     results = []
     start_time = time.time()
